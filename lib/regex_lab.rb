d48493928e010def91162cd11610df69b97537ba
Judy McGoldrick <jjeanius@verizon.net>
2017-12-29 16:41:21 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,19 @@
-def starts_with_a_vowel?(word)
+require 'pry'
 
+def starts_with_a_vowel?(word)
+  if word.match(/^[aeiou]/i)
+    true
+  else
+    false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  if text.scan(/un\w+ing\b/i)
+    true
+  else
+    false
+  end
 end
 
 def words_five_letters_long(text)
